c48453c53af253c35a9b36d4076d86794b9360d1
joaomotz <106111080+joaomotz@users.noreply.github.com>
2022-10-28 15:04:31 -0300

    Código Limpo para Poder Começar as Telas

---
+++ Src/Componentes/Menu/Index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Menu = () => {
+    return ( 
+        <View>
+            Menu
+        </View>
+     );
+}
+ 
+export default Menu;
