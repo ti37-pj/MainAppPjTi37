60ee5c39ae824f813eb64557407c9d36c7f46643
joaomotz <106111080+joaomotz@users.noreply.github.com>
2022-10-28 16:06:19 -0300

    Navigation Instalado

--- Src/Componentes/Menu/Index.js
+++ Src/Componentes/Menu/Index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 const Menu = () => {
     return ( 
         <View>
-            Menu
+            <Text>
+                Menu
+            </Text>       
         </View>
      );
 }
