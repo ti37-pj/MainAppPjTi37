8e0bc3cd9e12bb82b30d42098e3b8b61e4a9c947
joaomotz <106111080+joaomotz@users.noreply.github.com>
2022-10-28 16:31:01 -0300

    Inicio da Tela Cardapio

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native';
 import Menu from './Src/Componentes/Menu/Index';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
+	
   	return (
-		<View>
-			<Menu/>
-		</View>
-  	);
+		<NavigationContainer>
+			<Drawer.Navigator>
+				<Drawer.Screen name="Cadarpio" component={Menu} />
+			</Drawer.Navigator>
+		</NavigationContainer>
+	)
 }
 
 const e = StyleSheet.create({
