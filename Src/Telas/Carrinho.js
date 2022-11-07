83fc082d2a75dc1a22c2e17f711cbc32140159c1
joaomotz <106111080+joaomotz@users.noreply.github.com>
2022-11-07 16:15:17 -0300

    Botão de aumentar e diminuir quantidade de produtos funcionando

--- Src/Telas/Carrinho.js
+++ Src/Telas/Carrinho.js
@@ -1,34 +1,97 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 const Carrinho = () => {
 
-    const [ num, setNum] = React.useState(0);
+    useEffect(()=>{
+        atualizaTotal()
+    },produtosCarrinho)
+
+    const banco = [
+		{
+			id:1,
+			nome:'Alguma Coisa',
+			preco_venda:29,
+            quantidade:1,
+            total:29,
+		},
+		{
+			id:2,
+			nome:'Uma Coisa Diferente',
+			preco_venda:39,
+            quantidade:1,
+            total:39,
+		},
+		{
+			id:3,
+			nome:'Uma Coisa Ainda mais Diferente',
+			preco_venda:49,
+            quantidade:1,
+            total:49,
+		}
+	]
+
+    const [produtosCarrinho, setProdutosCarrinho] = React.useState(banco)
+    const [totalCarrinho, setTotalCarrinho] = React.useState(0)
+
+    const atualizaTotal = () => {
+        let precoTotal = 0
+        produtosCarrinho.map(c=>{
+            precoTotal += c.total
+        })
+        setTotalCarrinho(precoTotal)
+    }
     
-    const mudaNum = (contador) => {
-        const numero = contador == false ? (num-1) : (num+1);
-        setNum(numero)
+    const contador = (produto,contado) => {
+
+        produto.quantidade += contado == false ? -1: 1;
+        produto.quantidade = produto.quantidade < 1 ? 1 : produto.quantidade
+        
+        produto.total = produto.quantidade * produto.preco_venda
+
+        const novoCarrinho = produtosCarrinho.map(c=>{
+            if (c.id == produto.id){
+                return produto
+            }
+            return c
+        })
+
+        setProdutosCarrinho(novoCarrinho)
+       // atualizaTotal()
+
     }
 
-    return ( 
-        <View style={e.container} >
-            <Image style={e.img}  source={{uri:"https://via.placeholder.com/70x100"}} />
-            <View style={e.containerInfo} >
-                <Text style={e.text} >Nome do Produto</Text>
-                <Text style={e.preco} >R$:29,90</Text>
-                <View style={e.btnView} >
-                    <TouchableOpacity onPress={()=> mudaNum(false)} >
-                        <Text style={e.btnRound}>-</Text>
-                    </TouchableOpacity>
-                    <Text>{num}</Text>
-                    <TouchableOpacity onPress={()=> mudaNum(true)} >
-                        <Text style={e.btnRound}>+</Text>
-                    </TouchableOpacity>  
-                    <TouchableOpacity>
-                        <Text>Remover</Text>
-                    </TouchableOpacity>
-                </View> 
-            </View>
+    return (
+        <View>
+            {
+                produtosCarrinho == 0 ? <Text>Nenhum Produto no Carrinho</Text>:
+                <View>
+                {   
+                    produtosCarrinho.map(c =>{
+                    return<View style={e.container} key={c.id} >
+                        <Image style={e.img}  source={{uri:"https://via.placeholder.com/70x100"}} />
+                        <View style={e.containerInfo} >
+                            <Text style={e.text}>{c.nome}</Text>
+                            <Text style={e.preco} >R$:{c.total}</Text>
+                            <View style={e.btnView} >
+                                <TouchableOpacity onPress={()=> contador(c,false)} >
+                                    <Text style={e.btnRound}>-</Text>
+                                </TouchableOpacity>
+                                <Text>{c.quantidade}</Text>
+                                <TouchableOpacity onPress={()=> contador(c,true)} >
+                                    <Text style={e.btnRound}>+</Text>
+                                </TouchableOpacity>  
+                                <TouchableOpacity>
+                                    <Text>Remover</Text>
+                                </TouchableOpacity>
+                            </View> 
+                        </View>
+                    </View>
+                    })
+                }
+                </View>
+            }
+            <Text>Preço do Pedido:R${totalCarrinho}</Text>
         </View>
      );
 }
@@ -66,7 +129,7 @@ const e = StyleSheet.create({
     },
     img:{
         width:70,
-        height:85,
+        height:100,
     },
     btnRound:{
         backgroundColor:"#009688",
