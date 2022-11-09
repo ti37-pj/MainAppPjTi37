a51b9994407ec553eeddc1ddb5db9064d96196eb
Kauaflp <fillipie11@gmail.com>
2022-11-09 17:15:01 -0300

    Adição das Novas Telas

--- App.js
+++ App.js
@@ -2,17 +2,29 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 import Cardapio from './Src/Telas/Cardapio';
+import Cadastro from './Src/Telas/Cadastro';
+import Login from './Src/Telas/Login';
+import Mesa from './Src/Telas/Mesa';
+import { UsuarioProvedor } from './Src/Contexto/UsuarioContexto';
+import { MesaProvedor } from './Src/Contexto/MesaContexto';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
 	
   	return (
-		<NavigationContainer>
-			<Drawer.Navigator>			
-					<Drawer.Screen name="Cadarpio" component={Cardapio} />
-			</Drawer.Navigator>
-		</NavigationContainer>
+		<UsuarioProvedor>
+			<MesaProvedor>
+				<NavigationContainer>
+					<Drawer.Navigator>			
+							<Drawer.Screen name="Login" component={Login} />
+							<Drawer.Screen name="Mesa" component={Mesa} />
+							<Drawer.Screen name="Cadastro" component={Cadastro} />
+							<Drawer.Screen name="Cardápio" component={Cardapio} />
+					</Drawer.Navigator>
+				</NavigationContainer>
+			</MesaProvedor>
+		</UsuarioProvedor>
 	)
 }
 
