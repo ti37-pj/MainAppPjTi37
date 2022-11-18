f36fb6da5a5c33c4e17ebd9d019b7b08afc0d4d1
matheushlopes3 <106110898+matheushlopes3@users.noreply.github.com>
2022-11-18 16:24:29 -0300

    Customização da Tela de Pedido

--- Src/Telas/Menu.js
+++ Src/Telas/Menu.js
@@ -10,15 +10,12 @@ import { UsuarioContexto } from '../Contexto/UsuarioContexto';
 import { ProdutoProvedor } from '../Contexto/ProdutosContexto';
 import { CategoriaProvedor } from '../Contexto/CategoriaContexto';
 import { CarrinhoProvedor } from '../Contexto/CarrinhoContexto';
-<<<<<<< HEAD
 import { PedidoProvedor } from "../Contexto/PedidoContexto";
-
-=======
 import Carrinho from "./Carrinho";
 import Mesa from "./Mesa";
 import { MesaContexto } from "../Contexto/MesaContexto";
 import Pedido from "./Pedido";
->>>>>>> cd433b9db152610c6ba07d59e2068660e88255ed
+
 
 const Drawer = createDrawerNavigator();
 
@@ -31,23 +28,7 @@ const Menu = ()=>{
             <ProdutoProvedor>
                 <CategoriaProvedor>
                     <CarrinhoProvedor>
-<<<<<<< HEAD
                         <PedidoProvedor>
-                            <NavigationContainer>
-                                <Drawer.Navigator>
-                                    {usuario.id == undefined ?(
-                                        <>
-                                            <Drawer.Screen name="Pedido" component={Pedido} />  
-                                            <Drawer.Screen name="Status" component={Status} />     
-                                        </> 
-                                            ): (
-                                                <>
-                                        <Drawer.Screen name="Login" component={Login} />
-                                        <Drawer.Screen name="Cardápio" component={Cardapio} />
-                                        <Drawer.Screen name="Carrinho" component={Carrinho} />
-                                    </>
-                                    )
-=======
                         <NavigationContainer> 
                             <Drawer.Navigator>
                                 {usuario.id == undefined ?(
@@ -62,9 +43,7 @@ const Menu = ()=>{
                                         )}
                                         <Drawer.Screen name="Pedido" component={Pedido} />
                                     </>
-                                )
->>>>>>> cd433b9db152610c6ba07d59e2068660e88255ed
-                                }			
+                                )}
                                 </Drawer.Navigator>
                             </NavigationContainer>
                         </PedidoProvedor>                           
